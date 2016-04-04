4fcfcfa60e5759ec05d083cac9ad98a1a3b3dbd7
Superhuman07 <bhatiarahul51@gmail.com>
2016-04-05 00:45:41 +0530

    Fifth Commit

--- app/src/main/java/com/example/admin/workout/MainActivity.java
+++ app/src/main/java/com/example/admin/workout/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.admin.workout;
 
 import android.app.Activity;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity implements WorkoutListFragment.WorkoutListener {
 
@@ -14,13 +16,22 @@ public class MainActivity extends Activity implements WorkoutListFragment.Workou
     }
     @Override
     public void itemClicked(long id) {
-        WorkoutDetailFragment details = new WorkoutDetailFragment();
-        FragmentTransaction ft = getFragmentManager().beginTransaction();
-        details.setWorkout(id);
-        ft.replace(R.id.fragment_container, details);
-        ft.addToBackStack(null);
-        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-        ft.commit();
+        // large screen check whether fragment container exists or not!
+        View fragmentContainer = findViewById(R.id.fragment_container);
+        if (fragmentContainer != null) {
+            WorkoutDetailFragment details = new WorkoutDetailFragment();
+            FragmentTransaction ft = getFragmentManager().beginTransaction();
+            details.setWorkout(id);
+            ft.replace(R.id.fragment_container, details);
+            ft.addToBackStack(null);
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            ft.commit();
+        }
+        else {
+            Intent intent = new Intent(this, DetailActivity.class);
+            intent.putExtra(DetailActivity.EXTRA_WORKOUT_ID, (int)id);
+            startActivity(intent);
+        }
     }
 
 }
